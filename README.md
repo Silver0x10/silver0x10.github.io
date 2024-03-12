c3338f51f9d915752b6a7b39290ac2ee951e8b04
Silver0x10 <silverio.manganaro@gmail.com>
2024-03-12 01:27:32 +0100

    projects section

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Hi!
 This should be my personal page. \
-The idea is to create a porfolio with the best projects I've worked on so far. 
+The idea is to create a porfolio to frame the best projects I've worked on so far. 
 
 # ToDOs
 - [X] Icone e link navigation bar
 - [X] Rotating 3D donut
-- [ ] Projects Section
+- [X] Projects Section
+- [ ] Projects Canvas
+- [ ] Scrolling animation
 - [ ] Optimize for Google search
 - [ ] CV download
 - [ ] Writing effect
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const yellow = new THREE.Color(0xdea30e);
 const purple = new THREE.Color(0x6e00cc);
@@ -24,17 +25,20 @@ renderer.setSize(window.innerWidth, window.innerHeight); // set the size of the 
 
 camera.position.setZ(10); // move the camera in the z direction
 
-renderer.render(scene, camera); // render the scene from the perspective of the camera
-
 const light = new THREE.PointLight(0xaaaaaa, 700);
 light.position.set(-15,-10,15);
 scene.add(light);
 
+// // Helpers
+// const controls = new OrbitControls(camera, renderer.domElement);
 // const pointLightHelper = new THREE.PointLightHelper(light);
 // scene.add(pointLightHelper);
+// const gridHelper = new THREE.GridHelper(100, 10);
+// scene.add(gridHelper)
 
 let donut = new THREE.Object3D();
-let donut_scale = 20;
+let donut_scale = 19;
+// let donut_scale = 20;
 
 const loader = new GLTFLoader();
 loader.load( donut_path, function ( model ) { 
@@ -45,8 +49,16 @@ loader.load( donut_path, function ( model ) {
     donut.rotation.z = Math.PI / 6;
     scene.add(donut); 
 }, undefined, function ( error ) { console.error( error ); } );
+  
+renderer.render(scene, camera); // render the scene from the perspective of the camera
 
-renderer.render(scene, camera);
+
+function onWindowResize() {
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight); // set the size of the rendered view to the size of the window
+}
+window.addEventListener('resize', onWindowResize, false);
+
 
 function animate() {
     requestAnimationFrame(animate);
@@ -55,7 +67,8 @@ function animate() {
     donut.rotation.y += -0.001;
     donut.rotation.z += 0.001;
 
+    // controls.update();
+
     renderer.render(scene, camera);
 }
-
 animate();
