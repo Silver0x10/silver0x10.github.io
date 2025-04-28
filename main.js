2ddebd1bb5963a96ce33bcb1cfe82140ec8aa503
Silver0x10 <silverio.manganaro@gmail.com>
2025-04-29 00:18:58 +0200

    sparse cleanings and text refinements

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const yellow = new THREE.Color(0xdea30e);
 const purple = new THREE.Color(0x6e00cc);
@@ -30,16 +29,8 @@ const light = new THREE.PointLight(0xaaaaaa, 700);
 light.position.set(-15,-10,15);
 scene.add(light);
 
-// // Helpers
-// const controls = new OrbitControls(camera, renderer.domElement);
-// const pointLightHelper = new THREE.PointLightHelper(light);
-// scene.add(pointLightHelper);
-// const gridHelper = new THREE.GridHelper(100, 10);
-// scene.add(gridHelper)
-
 let donut = new THREE.Object3D();
 let donut_scale = 19;
-// let donut_scale = 20;
 
 const loader = new GLTFLoader();
 loader.load( donut_path, function ( model ) { 
@@ -64,7 +55,6 @@ function animate() {
     donut.rotation.z += 0.001;
 
     donutXVel *= 0.99;
-    // controls.update();
 
     renderer.render(scene, camera);
 }
